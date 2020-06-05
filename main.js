ee7ef768f9d62ae77e839fec75c9353dbaf36161
Rohit Bhandari <bhandarirohit080@gmail.com>
2020-06-05 13:01:24 +0530

    Type and speak files

---
+++ main.js
@@ -0,0 +1,103 @@
+// Init SpeechSynth API
+const synth = window.speechSynthesis;
+
+// DOM ELEMENTS
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
+const body = document.querySelector('body');
+
+// Init voices array
+let voices= [];
+
+const getVoices = () => {
+  voices= synth.getVoices();
+
+  // Loop through voices and create an option for each one
+  voices.forEach(voice => {
+    // create an option ELEMENT
+    const option = document.createElement('option');
+    // fill option with voice and lang
+    option.textContent = voice.name + '('+ voice.lang + ')';
+
+    // set needed attributes
+    option.setAttribute('data-lang', voice.lang);
+    option.setAttribute('data-name', voice.name);
+    voiceSelect.appendChild(option);
+  });
+};
+
+getVoices();
+if(synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
+
+
+// Speak
+
+const speak = () =>{
+
+    // check if speaking
+    if(synth.speaking) {
+      console.error('Already Speaking...');
+      return;
+    }
+    if(textInput.value !== '') {
+
+      // Add background animation
+      body.style.background = '#141414 url(wave.gif)';
+      body.style.backgroundRepeat = 'repeat-x';
+      body.style.backgroundSize = '100% 100%';
+
+      // get speak text
+      const speakText = new SpeechSynthesisUtterance(textInput.value);
+      // speak end
+      speakText.onend = e => {
+        console.log('Done Speaking...');
+        body.style.background = '#141414';
+      }
+
+      // speak error
+      speakText.onerror = e => {
+        console.log('Something went wrong...');
+      }
+
+      // Selected Voice
+      const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+      // loop through voices
+      voices.forEach(voice => {
+        if (voice.name === selectedVoice) {
+          speakText.voice = voice;
+        }
+      });
+
+      // Set pitch and rate
+      speakText.rate = rate.value;
+      speakText.pitch = pitch.value;
+      // speak
+      synth.speak(speakText);
+    }
+};
+
+// Event Listners
+
+// text form submit
+textForm.addEventListener('submit', e => {
+  e.preventDefault();
+  speak();
+  textInput.blur();
+});
+
+// Rate value change
+rate.addEventListener('change', e => (rateValue.textContent = rate.value));
+
+// pitch value change
+pitch.addEventListener('change', e => (pitchValue.textContent = pitch.value));
+
+// Voice select change
+voiceSelect.addEventListener('change', e => speak());
